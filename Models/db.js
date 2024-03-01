8fda3aacadf457fd6ae6a3deebdd5c6c839fe11b
proabdulbasit <proabdulbasit.me@gmail.com>
2024-03-01 11:17:20 +0500

    vercel code

--- Models/db.js
+++ Models/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose"
 //mongoose.Promise = global.Promise ;
 
-const db=`mongodb+srv://dilipwannigamage:btFVeAwj0ImW4LkC@spotiviraldb.rkamkht.mongodb.net/?retryWrites=true&w=majority`
+// const db=`mongodb+srv://dilipwannigamage:btFVeAwj0ImW4LkC@spotiviraldb.rkamkht.mongodb.net/?retryWrites=true&w=majority`
+const db=`mongodb+srv://basit:admin@cluster0.w54f2vf.mongodb.net/`
 
 export const connection=async()=>{ return await mongoose.connect(db).then(() => {
       console.log('Connected to MongoDB');
